004037923c262448af4596bf7af13b29a1f9a924
davidOrbang <orbangdavid@yahoo.com>
2023-05-10 16:34:38 +0100

    4.5*: helper functions and unit tests, step3

--- tests/blog.test.ts
+++ tests/blog.test.ts
@@ -1,8 +1,40 @@
-import dummy from '../utils/list_helper.js';
+import lisHelper from '../utils/list_helper.js';
 
-test('dummy return one', () => {
-	const blogs = [];
-	const result = dummy(blogs);
-	expect(result).toBe(1);
+describe('Dummy', () => {
+	test('dummy return one', () => {
+		const blogs = [];
+		const result = lisHelper.dummy(blogs);
+		expect(result).toBe(1);
+	});
 });
 
+describe('Total likes', () => {
+	test('of empty list is 0', () => {
+		const {emptyBlog} = lisHelper;
+		const totalLikes = lisHelper.totalLikes(emptyBlog);
+		expect(totalLikes).toBe(0);
+	});
+	test('when list hsa only one posting', () => {
+		const {oneBlog} = lisHelper;
+		const totalLikes = lisHelper.totalLikes(oneBlog);
+		expect(totalLikes).toBe(7);
+	});
+	test('of a large list benig calculated', () => {
+		const {bigBlogs} = lisHelper;
+		const totalLikes = lisHelper.totalLikes(bigBlogs);
+		expect(totalLikes).toBe(36);
+	});
+});
+
+describe('Favourite blog', () => {
+	test('favourtie === highest likes', () => {
+		const {bigBlogs} = lisHelper;
+		const favourtieBlog = lisHelper.favourtieBlog(bigBlogs);
+		expect(favourtieBlog).toEqual({_id: '5a422b3a1b54a676234d17f9',
+			title: 'Canonical string reduction',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+			likes: 12,
+			__v: 0});
+	});
+});
